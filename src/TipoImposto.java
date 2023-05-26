3b3fe0447639726f0163219553ace9620acea2e8
odevpedro <pedrosschmidt2@gmail.com>
2023-05-25 21:30:58 -0300

    solução strategy

--- src/TipoImposto.java
+++
@@ -1,7 +0,0 @@
-import java.nio.file.AtomicMoveNotSupportedException;
-
-public enum TipoImposto {
-    ICMS,
-    ISS
-
-}
--- src/br/com/odevpedro/loja/imposto/Main.java
+++ src/br/com/odevpedro/loja/imposto/Main.java
@@ -1,3 +1,5 @@
+package br.com.odevpedro.loja.imposto;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
